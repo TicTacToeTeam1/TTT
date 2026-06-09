49f8cec9166027c0f6219d3bc8c74cd9a3597a85
Ivanna Caraccio Ayala <47951689+IvannaRCA@users.noreply.github.com>
2026-06-09 14:54:17 +0200

    Ttt 2 inicializacion del tablero (#2)
    * creacion d etablero
    
    * tablero
    
    * eliminar carpeta target
    
    * update tablero. Change language to english
    
    ---------
    
    Co-authored-by: nmantilla12 <nnnmantillam@gmail.com>

--- src/main/java/com/femcoders-team1/App.java
+++
@@ -1,9 +0,0 @@
-package com.femcoders-team1;
-
-public class App 
-{
-    public static void main( String[] args )
-    {
-       
-    }
-}
---
+++ src/main/java/com/team1/App.java
@@ -0,0 +1,13 @@
+package com.team1;
+
+
+public class App {
+
+    public static void main(String[] args) {
+        
+        Board board = new Board();
+        board.printBoard();
+
+
+    }
+}
