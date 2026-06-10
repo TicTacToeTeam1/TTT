24f36f34a29972e2aad2517c587bf48095bd4681
Ivanna Caraccio <ivacaraccio@hotmail.com>
2026-06-10 13:03:00 +0200

    feat: player creations, welcome messages and loop game

--- src/main/java/com/team1/App.java
+++ src/main/java/com/team1/App.java
@@ -1,13 +1,47 @@
 package com.team1;
 
+import java.util.Scanner;
 
 public class App {
 
     public static void main(String[] args) {
         
+        Scanner scanner = new Scanner(System.in);
+
         Board board = new Board();
-        board.printBoard();
+        
+
+        // WELCOME
+        System.out.println("Juguemos tic tac toe");
+        System.out.println("Objetivo: en un tablero de 3x3, debes completar con 3 símbolos (X o O) una fila, columna o diagonal para ganar el juego.");
+        
+        System.out.print("Introduce un nombre para la primera jugadora: ");
+        String name = scanner.nextLine();
+        Player player1 = new Player('X', name);
+        
+        System.out.print("Introduce un nombre para la segunda jugadora: ");
+        name = scanner.nextLine();
+        Player player2 = new Player('O', name);
+        
+        System.out.println("Jugadora " + player1.getName() + " eres " + player1.getToken());
+        System.out.println("Jugadora " + player2.getName() + " eres " + player2.getToken());
 
 
+        // GAME
+        boolean game = true;
+
+        do {
+            System.out.println("Turno de: " + player1.getName());
+            System.out.print("Selecciona una fila y columna: ");
+            String field = scanner.nextLine();
+            board.printBoard();
+            
+        } while(game);
+        
+        
+        // END
+
+        scanner.close();
+        
     }
 }
